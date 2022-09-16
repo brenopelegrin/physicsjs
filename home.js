4fad50dcdc7d8dc2e3c1b211e05b60cdbba12371
brenopelegrin <breno.pelegrin@usp.br>
2022-09-16 09:16:50 -0300

    create status struct

--- home.js
+++ home.js
@@ -1,12 +1,27 @@
-let status_movimento3d_gui = true;
+//let status_movimento3d_gui = true;
+const status_obj = {
+    movimento3d:{
+        gui: true,
+        api: true
+    },
+    rocketlaunch:{
+        gui: false,
+        api: false
+    }
+};
 
 const btn_movimento3d_gui = document.getElementById("btn_movimento3d_gui");
+const btn_movimento3d_api = document.getElementById("btn_movimento3d_api");
 
-if (status_movimento3d_gui){
+if (status_obj.movimento3d.gui){
     btn_movimento3d_gui.className="active";
     btn_movimento3d_gui.onclick = function () {
         window.open("./movimento3D.html");
     };
+}
+
+if (status_obj.movimento3d.api){
+    btn_movimento3d_api.className="active";
 }
 
 
