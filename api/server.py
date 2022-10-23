ace560a582882ee3bbeefb05649c1f476f5aa861
brenopelegrin <breno.pelegrin@usp.br>
2022-10-23 09:20:43 -0300

    trying to use heroku postgres

--- api/server.py
+++ api/server.py
@@ -2,12 +2,13 @@ from flask import Flask, request, Response
 from sqlalchemy import UniqueConstraint, JSON, func
 from flask_restful import reqparse, abort, Api, Resource
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 api = Api(app)
 db = SQLAlchemy(app)
