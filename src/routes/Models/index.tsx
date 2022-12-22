53b94fe6679962069d98a0b71be0a9c2a6bb7a47
brenopelegrin <breno.pelegrin@usp.br>
2022-12-22 14:08:00 -0300

    Modified routes to support vercel

--- src/routes/Models/index.tsx
+++ src/routes/Models/index.tsx
@@ -7,18 +7,18 @@ export default function ModelsPage(){
         <WrapItem>
           <Card url_api='https://github.com/brenopelegrin/physicsjs/tree/master/backend'
                 url_gui='/static/movimento3D.html' 
-                title='mov3d' 
+                title='mov3d (legacy)' 
                 subtitle='Cinemática em 3D' 
                 description='Simule o movimento de uma esfera em três dimensões. Grandezas variáveis: massa, raio, velocidade inicial, posição inicial. Documentação da API no GitHub' 
                 tags={['knematics', 'air resistance']}/>
         </WrapItem>
         <WrapItem>
         <Card   url_api='#'
-                url_gui='#' 
-                title='Indisponível' 
-                subtitle='indisponível' 
-                description='Ainda não há nada aqui :(' 
-                tags={['none']}/>
+                url_gui='/models/mov3d' 
+                title='mov3d (new - dev)' 
+                subtitle='Cinemática em 3D' 
+                description='Simule o movimento de uma esfera em três dimensões. Grandezas variáveis: massa, raio, velocidade inicial, posição inicial. Documentação da API no GitHub' 
+                tags={['knematics', 'air resistance', 'dev']}/>
         </WrapItem>
         <WrapItem>
         <Card   url_api='#'
